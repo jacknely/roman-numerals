3cbd45944f82caf764acdd34af057f9da8a4fa4e
jacknely <jackn926@gmail.com>
2019-07-21 17:21:11 +0100

    added doc strings

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -26,12 +26,24 @@ class TestRomanNumerals:
 
     @pytest.mark.parametrize('roman, result', test_data)
     def test_convert_numerals(self, roman, result):
+        """
+        Parameterised test of the covert numerals function
+        in class Numerals
+        :param roman: Input data as roman numerals
+        :param result: Output as integer
+        """
         self.test_conversion.digits = roman
         test_number_output = self.test_conversion.convert_numerals()
         assert test_number_output == result
 
     @pytest.mark.parametrize('char', test_error_str)
     def test_number_check(self, char):
+        """
+        Parameterised test of the number check method
+        in the class Numerals for string char
+        :param char: Input of a char that is not
+        roman numeral
+        """
         with pytest.raises(ValueError) as excinfo:
             self.test_conversion.digits = char
             self.test_conversion.numeral_check()
@@ -39,6 +51,11 @@ class TestRomanNumerals:
 
     @pytest.mark.parametrize('digit', test_error_dig)
     def test_number_check(self, digit):
+        """
+        Parameterised test of the number check method
+        in the class Numerals for digits
+        :param digit: Input of a digit
+        """
         with pytest.raises(ValueError) as excinfo:
             self.test_conversion.digits = digit
             self.test_conversion.numeral_check()
