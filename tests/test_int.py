b11e1f812fb7395311b1036a72b12da22ad25e5c
jacknely <jackn926@gmail.com>
2019-07-21 17:25:03 +0100

    added doc strings

--- tests/test_int.py
+++ tests/test_int.py
@@ -13,11 +13,17 @@ class TestRomanNumerals:
 
     @pytest.mark.parametrize('roman, result', test_data)
     def test_main_app(self, roman, result):
+        """
+        Parameterised test of the main method of application
+        """
         result = run.Converter.main(roman)
         assert result == result
 
     @pytest.mark.parametrize('roman, result', test_data)
     def test_main_input(self, roman, result, monkeypatch):
+        """
+        Parameterised test of sys inputs from command line
+        """
         with monkeypatch.context() as m:
             m.setattr(sys, 'argv', ['run', roman])
             assert run.main() == result
